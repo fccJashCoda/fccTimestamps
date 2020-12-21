ce7e271a5476bc0a3aa6d8cb0affc6d3908f716c
fccJashCoda <personalrevolution2014@gmail.com>
2020-12-22 00:14:34 +0100

    working solution

--- server.js
+++ server.js
@@ -6,25 +6,42 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const e = require('express');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/timestamp/:time?', (req, res) => {
+  Date.prototype.isValid = function () {
+    return this.getTime() === this.getTime();
+  };
 
+  let date;
+  if (!req.params.time) {
+    date = new Date();
+  } else if (new Date(req.params.time).isValid()) {
+    date = new Date(req.params.time);
+  } else if (new Date(Number(req.params.time)).isValid()) {
+    date = new Date(Number(req.params.time));
+  } else {
+    return res.json({ error: 'Invalid Date' });
+  }
+
+  return res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
